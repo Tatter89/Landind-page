1c95a23aec5d5db6e842f1f9df14fd7887f681d0
ZsoltMarta <taticsa89@gmail.com>
2023-10-05 15:37:01 +0200

    Btns for the slider

--- script.js
+++ script.js
@@ -39,135 +39,6 @@ const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
 const nav = document.querySelector(".nav");
 
-/* // FAQ Button
-
-const faqBoxes = document.querySelectorAll(".faq-box");
-const faqIcons = document.querySelectorAll(".fa-chevron-down");
-
-faqBoxes.forEach((faqBox) => {
-  faqBox.addEventListener("click", (event) => {
-    faqBox.classList.toggle("active");
-  });
-}); */
-
-//Show menu
-
-/* let showMenu = false;
-
-menuBtn.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-  if (!showMenu) {
-    hamburger.classList.add("open");
-    nav.classList.add("open");
-
-    showMenu = true;
-  } else {
-    hamburger.classList.remove("open");
-    nav.classList.remove("open");
-
-    showMenu = false;
-  }
-} */
-
-// Nav offset
-
-/* let previousScrollPosition = window.pageYOffset;
-
-const displayNavOnScroll = () => {
-  const currentScrollPosition = window.pageYOffset;
-  const nav = document.querySelector(".nav");
-  if (previousScrollPosition > currentScrollPosition) {
-    nav.style.paddingTop = "15px";
-    nav.style.paddingBottom = "15px";
-  } else {
-    nav.style.paddingTop = "5px";
-    nav.style.paddingBottom = "5px";
-  }
-  previousScrollPosition = currentScrollPosition;
-};
-
-document.addEventListener("scroll", displayNavOnScroll); */
-
-// Dragable slider
-
-/* const carousel = document.querySelector(".carousel"),
-  firstImg = carousel.querySelectorAll("img")[0];
-arrowIcons = document.querySelectorAll(".wrapper i");
-
-let isDragStart = false,
-  isDragging = false,
-  prevPageX,
-  prevSrollLeft,
-  positionDiff;
-
-const showHideIcons = () => {
-  let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
-
-  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-  arrowIcons[1].style.display =
-    carousel.scrollLeft == scrollWidth ? "none" : "block";
-};
-
-arrowIcons.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    let firstImgWidth = firstImg.clientWidth + 14;
-    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-    setTimeout(() => showHideIcons(), 60);
-  });
-});
-
-const autoSlide = () => {
-  if (carousel.scrollLeft == carousel.scrollWidth - carousel.clientWidth)
-    return;
-
-  positionDiff = Math.abs(positionDiff);
-  let firstImgWidth = firstImg.clientWidth + 14;
-  let valDifference = firstImgWidth - positionDiff;
-
-  if (carousel.scrollLeft > prevSrollLeft) {
-    return (carousel.scrollLeft +=
-      positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
-  }
-  carousel.scrollLeft -=
-    positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-};
-
-const dragStart = (e) => {
-  isDragStart = true;
-  prevPageX = e.pageX || e.touches[0].pageX;
-  prevSrollLeft = carousel.scrollLeft;
-};
-
-const dragging = (e) => {
-  if (!isDragStart) return;
-  e.preventDefault();
-  isDragging = true;
-  carousel.classList.add("dragging");
-  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-  carousel.scrollLeft = prevSrollLeft - positionDiff;
-  showHideIcons();
-};
-
-const dragStop = () => {
-  isDragStart = false;
-  carousel.classList.remove("dragging");
-
-  if (!isDragging) return;
-  isDragging = false;
-  autoSlide();
-};
-
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
-
-carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
-
-carousel.addEventListener("mouseup", dragStop);
-carousel.addEventListener("mouseleave", dragStop);
-carousel.addEventListener("touchend", dragStop); */
-
 const imageCarousel = (carouselElement) => {
   let activeIndex = 0;
   const prevButton = carouselElement.querySelector(
@@ -179,31 +50,66 @@ const imageCarousel = (carouselElement) => {
   const slides = carouselElement.getElementsByClassName(
     "image-carousel__slide"
   );
+  const dotsContainer = carouselElement.querySelector(
+    ".image-carousel__dots-container"
+  );
+  const handleDotClick = (event) => {
+    slides[activeIndex].classList.remove("image-carousel__slide--active");
+    dots[activeIndex].classList.remove("image-carousel__dot--active");
+    activeIndex = +event.target.dataset.index;
+    slides[event.target.dataset.index].classList.add(
+      "image-carousel__slide--active"
+    );
+    dots[event.target.dataset.index].classList.add(
+      "image-carousel__dot--active"
+    );
+    console.log("dot", activeIndex);
+  };
 
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("image-carousel__dot");
+    dot.dataset.index = i;
+    dot.addEventListener("click", handleDotClick);
+    dotsContainer.appendChild(dot);
+  }
+
+  const dots = carouselElement.getElementsByClassName("image-carousel__dot");
+  dots[activeIndex].classList.add("image-carousel__dot--active");
   slides[activeIndex].classList.add("image-carousel__slide--active");
 
   const nextSlide = () => {
     if (activeIndex < slides.length - 1) {
       slides[activeIndex].classList.remove("image-carousel__slide--active");
       slides[activeIndex + 1].classList.add("image-carousel__slide--active");
+      dots[activeIndex].classList.remove("image-carousel__dot--active");
+      dots[activeIndex + 1].classList.add("image-carousel__dot--active");
       activeIndex++;
     } else {
       slides[activeIndex].classList.remove("image-carousel__slide--active");
+      dots[activeIndex].classList.remove("image-carousel__dot--active");
       activeIndex = 0;
       slides[activeIndex].classList.add("image-carousel__slide--active");
+      dots[activeIndex].classList.add("image-carousel__dot--active");
     }
+    console.log("arrow", activeIndex);
   };
 
   const prevSlide = () => {
     if (activeIndex > 0) {
       slides[activeIndex].classList.remove("image-carousel__slide--active");
       slides[activeIndex - 1].classList.add("image-carousel__slide--active");
+      dots[activeIndex].classList.remove("image-carousel__dot--active");
+      dots[activeIndex - 1].classList.add("image-carousel__dot--active");
       activeIndex--;
     } else {
       slides[activeIndex].classList.remove("image-carousel__slide--active");
+      dots[activeIndex].classList.remove("image-carousel__dot--active");
       activeIndex = slides.length - 1;
       slides[activeIndex].classList.add("image-carousel__slide--active");
+      dots[activeIndex].classList.add("image-carousel__dot--active");
     }
+    console.log("arrow", activeIndex);
   };
 
   nextButton.addEventListener("click", () => nextSlide());
