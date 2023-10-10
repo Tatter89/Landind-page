557bc61b584cab09a9b8f12f7798e8d1ffce34ee
ZsoltMarta <taticsa89@gmail.com>
2023-10-10 17:31:42 +0200

    Instaload added

--- script.js
+++ script.js
@@ -138,30 +138,30 @@ function rightClick() {
 }
 
 // scrolling with buttons
-const upButton = document.querySelector('.scrollButton');
-const downButton = document.querySelector('.downButton');
+const upButton = document.querySelector(".scrollButton");
+const downButton = document.querySelector(".downButton");
 const scrollPageTo = (x = 0, y = 0) => window.scrollTo(x, y);
 
 const scrollButtonHandler = (event, scrollOffset = 600) => {
   if (window.scrollY > scrollOffset) {
     // show up button
-    upButton.classList.add('active');
-    if(downButton) downButton.classList.add('hidden');
+    upButton.classList.add("active");
+    if (downButton) downButton.classList.add("hidden");
   } else {
     // hide up button || show down button
-    upButton.classList.remove('active');
-    if(downButton) downButton.classList.remove('hidden');
+    upButton.classList.remove("active");
+    if (downButton) downButton.classList.remove("hidden");
   }
 };
 
-if(downButton) {
-  downButton.addEventListener('click', () => {
+if (downButton) {
+  downButton.addEventListener("click", () => {
     scrollPageTo(0, 1000);
   });
 }
-upButton.addEventListener('click', scrollPageTo);
-window.addEventListener('scroll', scrollButtonHandler);
-window.addEventListener('DOMContentLoaded', scrollButtonHandler);
+upButton.addEventListener("click", scrollPageTo);
+window.addEventListener("scroll", scrollButtonHandler);
+window.addEventListener("DOMContentLoaded", scrollButtonHandler);
 
 /* const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
